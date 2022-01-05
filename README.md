f27ffa88f2f1ec0d0ab5ba6a1b6d090982442cfb
Samuel Ko <koshunyin@gmail.com>
2022-01-05 22:33:21 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# weather-api
+# Weather API
+
+![licence badge](https://img.shields.io/github/license/samuelko123/weather-api)
+
+It is backend for my React Native project.
---
+++ pages/api/seven-day-forecast/[state].js
@@ -0,0 +1,25 @@
+import cities from '../../../utils/cities'
+
+export default async function handler(req, res) {
+  const { state } = req.query
+
+  if (Object.keys(cities).indexOf(state) === -1) {
+    res.status(404).json({ error: 'Not Found' })
+    return
+  }
+
+  const { lat, lon } = cities[state]
+  const url = `https://api.openweathermap.org/data/2.5/onecall?appid=${process.env.API_KEY}&lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts,current&units=metric`
+  const res_weather = await fetch(url)
+  const data = await res_weather.json()
+
+  const result = data.daily.map(item => {
+    return {
+      dt: item.dt,
+      min: item.temp.min,
+      max: item.temp.max,
+    }
+  })
+
+  res.status(200).json(result)
+}
