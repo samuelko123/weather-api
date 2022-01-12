6bc3257b7abbcd1709fa73c0085ac32a1b743a25
Samuel Ko <koshunyin@gmail.com>
2022-01-12 23:11:17 +1100

    add axios

--- pages/api/seven-day-forecast/[state].js
+++ pages/api/seven-day-forecast/[state].js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import cities from '../../../utils/cities'
 
 export default async function handler(req, res) {
@@ -10,8 +11,8 @@ export default async function handler(req, res) {
 
   const { lat, lon } = cities[state]
   const url = `https://api.openweathermap.org/data/2.5/onecall?appid=${process.env.API_KEY}&lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts,current&units=metric`
-  const res_weather = await fetch(url)
-  const data = await res_weather.json()
+  const res_weather = await axios.get(url)
+  const data = await res_weather.data
 
   const result = data.daily.map(item => {
     return {
