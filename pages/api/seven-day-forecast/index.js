5a650c6123b725c2f0fcb5ae6d20602c77a5916a
Samuel Ko <koshunyin@gmail.com>
2022-01-13 20:26:12 +1100

    add lat lon query

--- pages/api/seven-day-forecast/index.js
+++ pages/api/seven-day-forecast/index.js
@@ -1,16 +1,9 @@
 import axios from 'axios'
-import cities from '../../../utils/cities'
 
 export default async function handler(req, res) {
-  const { state } = req.query
+  const { lat, lon } = req.query
 
   try {
-    if (Object.keys(cities).indexOf(state) === -1) {
-      res.status(404).json({ error: 'Not Found' })
-      return
-    }
-
-    const { lat, lon } = cities[state]
     const url = `https://api.openweathermap.org/data/2.5/onecall?appid=${process.env.API_KEY_WEATHER}&lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts,current&units=metric`
     const res_weather = await axios.get(url)
     const data = await res_weather.data
