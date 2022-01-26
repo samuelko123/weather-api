3fa2489d531f22b34e905921e00ab5e8ca589699
Samuel Ko <koshunyin@gmail.com>
2022-01-26 11:33:27 +1100

    update api

--- pages/api/forecast/index.js
+++ pages/api/forecast/index.js
@@ -101,7 +101,6 @@ export default async function handler(req, res) {
 
         res.status(200).json(result)
     } catch (err) {
-        console.log(err)
         res.status(500).json({ error: err.toString() })
     }
 }
--- pages/api/suburbs/[q].js
+++ pages/api/suburbs/[q].js
@@ -4,33 +4,20 @@ export default async function handler(req, res) {
     const { q } = req.query
 
     try {
-        const url = `http://api.jsacreative.com.au/v1/suburbs?q=${q}`
-        const data = await fetchData(url)
-        res.status(200).json(data)
-    } catch (err) {
-        try {
-            const url = `http://v0.postcodeapi.com.au/suburbs.json?q=${q}`
-            const data = await fetchData(url)
-            res.status(200).json(result)
-        } catch (err) {
-            res.status(500).json({ error: err })
-        }
-    }
-}
-
-async function fetchData(url) {
-    const { data } = await axios.get(url)
-    const result = data
-        .filter(item => !!item.latitude && !!item.longitude)
-        .map(item => {
+        const url = `http://api.geonames.org/postalCodeSearchJSON?placename=${q}&maxRows=10&countryCode=AU&username=${process.env.API_KEY_SUBURB}`
+        const { data } = await axios.get(url)
+        const result = data.postalCodes.map(item => {
             return {
-                name: item.name,
-                lat: item.latitude,
-                lon: item.longitude,
-                postcode: item.postcode,
-                state: item.state.abbreviation,
+                name: item.placeName,
+                lat: item.lat,
+                lon: item.lng,
+                postcode: item.postalCode,
+                state: item['ISO3166-2'],
             }
         })
-    
-    return result
+
+        res.status(200).json(result)
+    } catch (err) {
+        res.status(500).json({ error: err.toString() })
+    }
 }
